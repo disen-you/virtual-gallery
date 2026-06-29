c63f49bf27f8c869c24b5b2daa7a165f1721c913
disen-you <disen.you19@gmail.com>
2026-06-29 16:17:16 -0400

    Update navbar.js

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,1 +1,15 @@
+const header = document.querySelector("header");
 
+window.addEventListener("scroll",()=>{
+
+    if(window.scrollY>60){
+
+        header.classList.add("scrolled");
+
+    }else{
+
+        header.classList.remove("scrolled");
+
+    }
+
+});
