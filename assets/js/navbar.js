9a4a44bbc5916311c705a2bbd8e437150907adfe
disen-you <disen.you19@gmail.com>
2026-07-01 15:40:26 -0400

    Update navbar.js for improved scroll handling
    Refactor scroll event listener to use optional chaining.

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,15 +1,9 @@
-const header = document.querySelector("header");
+const header = document.querySelector('header');
 
-window.addEventListener("scroll",()=>{
-
-    if(window.scrollY>60){
-
-        header.classList.add("scrolled");
-
-    }else{
-
-        header.classList.remove("scrolled");
-
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 60) {
+        header?.classList.add('scrolled');
+    } else {
+        header?.classList.remove('scrolled');
     }
-
 });
